00ab67d2878e7c45739767c1c829d4627038fc18
adnan <katmanfatahoni@gmail.com>
2024-08-03 22:28:11 +0700

    repair view in landing page bledeng

--- resources/views/catalog/list.blade.php
+++ resources/views/catalog/list.blade.php
@@ -3,30 +3,28 @@
 @section('title', 'Galery')
 
 @section('content')
-    {{-- <section class="section-card"> --}}
-        <div class="container mt-5">
-
-            <div class="row">
-                @foreach ($catalog as $c)
-                    <div class="col-md-4 mb-4">
-                        <div class="card">
-                            <img src="{{ asset('storage/images/' . $c->image) }}" class="card-img-top"
-                                alt="{{ $c->name }}" onclick="showImage('{{ asset('storage/images/' . $c->image) }}')">
-                            <div class="card-body">
-                                <h5 class="card-title">{{ $c->name }}</h5>
-                                <p class="card-text">{{ $c->description }}</p>
-                                <p class="card-text"><strong>Refresh Rate:</strong> {{ $c->freshrate }} Hz</p>
-                            </div>
-                        </div>
+<section class="section-card">
+    <div class="container">
+        <div class="d-flex flex-wrap">
+            @foreach ($catalog as $c)
+            <div class="w-25 px-2 py-2">
+                <div class="card h-100">
+                    <img src="{{ asset('storage/images/' . $c->image) }}" class="card-img-top" alt="{{ $c->name }}"
+                        onclick="showImage('{{ asset('storage/images/' . $c->image) }}')">
+                    <div class="card-body">
+                        <h5 class="card-title">{{ $c->name }}</h5>
+                        <p class="card-text">{{ $c->description }}</p>
+                        <p class="card-text"><strong>Refresh Rate:</strong> {{ $c->freshrate }} Hz</p>
                     </div>
-                @endforeach
-
+                </div>
             </div>
+            @endforeach
         </div>
-    </section>
+    </div>
+</section>
 @endsection
-    <script>
-        function showImage(url) {
-            window.open(url, '_blank');
-        }
-    </script>
+<script>
+    function showImage(url) {
+        window.open(url, '_blank');
+    }
+</script>
