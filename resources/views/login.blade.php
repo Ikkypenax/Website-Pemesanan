c14a12db98d4f3322960af96c20fdf91110795fe
Ikkypenax <ikkypena@gmail.com>
2024-09-10 16:15:16 +0700

    rom

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -10,11 +10,8 @@
 
 <body class="bg-gradient-primary">
     <div class="container">
-
         <div class="row justify-content-center">
-
             <div class="col-xl-5 col-lg-12 col-md-9">
-
                 <div class="card o-hidden border-0 shadow-lg my-5">
                     <div class="card-body p-0">
                         <div class="row">
