0a9b0b94748b68ad31f81be1703abfb4a60c3b12
Ikkypenax <ikkypena@gmail.com>
2024-10-08 23:42:03 +0700

    invoice

--- database/migrations/2024_08_28_083707_create_orders_table.php
+++ database/migrations/2024_08_28_083707_create_orders_table.php
@@ -13,6 +13,7 @@ return new class extends Migration
     {
         Schema::create('orders', function (Blueprint $table) {
             $table->id();
+            $table->string('order_code')->unique()->after('id')->nullable();
             $table->string('name');
             $table->string('wa');
             $table->string('regency');
