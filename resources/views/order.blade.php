7156f48c400814f9fcbd4e1af646c670a95ea980
Ikkypenax <ikkypena@gmail.com>
2024-10-09 07:51:40 +0700

    table

--- resources/views/order.blade.php
+++ resources/views/order.blade.php
@@ -106,31 +106,7 @@
         </form>
     </div>
 
-<!-- Success Modal -->
-<div class="modal fade" id="successModal" tabindex="-1" aria-labelledby="successModalLabel" aria-hidden="true">
-    <div class="modal-dialog modal-dialog-centered">
-        <div class="modal-content">
-            <div class="modal-header">
-                <h5 class="modal-title" id="successModalLabel">Notifikasi</h5>
-                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-            </div>
-            <div class="modal-body text-center">
-                <div class="checkmark-wrapper">
-                    <i class="fas fa-check-circle checkmark"></i>
-                </div>
-                <div>
-                    <strong>{{ session('success') }}</strong>
-                </div>
-            </div>
-            <div class="modal-footer">
-                {{-- <a href="{{ route('order.details') }}" class="btn btn-primary">Cek Pesanan</a> --}}
-                <a href="{{ route('order.details', ['order_code' => session('order_code')]) }}" class="btn btn-primary">Cek Pesanan</a>
 
-                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Kembali</button>
-            </div>
-        </div>
-    </div>
-</div>
 
 
     <!-- Alert Modal -->
@@ -202,120 +178,149 @@
                 </div>
             </div>
         </div>
+    </div>
 
-        <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+    <!-- Success Modal -->
+<div class="modal fade" id="successModal" tabindex="-1" aria-labelledby="successModalLabel" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="successModalLabel">Notifikasi</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body text-center">
+                <div class="checkmark-wrapper">
+                    <i class="fas fa-check-circle checkmark"></i>
+                </div>
+                <div>
+                    <strong>{{ session('success') }}</strong>
+                </div>
+            </div>
+            <div class="modal-footer">
+                @if (session('order_code'))
+                    <a href="{{ route('order.details', ['order_code' => session('order_code')]) }}" class="btn btn-primary">Cek Pesanan</a>
+                @else
+                    <button class="btn btn-secondary" disabled>Cek Pesanan</button>
+                @endif
+                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Kembali</button>
+            </div>
+        </div>
+    </div>
+</div>
 
-        <script>
-            $(document).ready(function() {
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
 
-                $('#provinces').on('change', function() {
-                    var province_id = $(this).val();
-                    if (province_id) {
-                        $.ajax({
-                            url: "{{ route('getRegencies') }}",
-                            type: "POST",
-                            data: {
-                                province_id: province_id,
-                                _token: '{{ csrf_token() }}'
-                            },
-                            success: function(data) {
-                                $('#regencies').empty().append(
-                                    '<option selected>Pilih Kabupaten</option>');
-                                $('#regencies').append(data);
-                            },
-                            error: function(xhr, status, error) {
-                                console.log(xhr.responseText);
-                            }
-                        });
-                    } else {
-                        $('#regencies').empty().append('<option selected>Pilih Kabupaten</option>');
-                    }
-                });
+    <script>
+        $(document).ready(function() {
 
-                $('#category').on('change', function() {
-                    var category_name = $(this).val();
-                    if (category_name) {
-                        $.ajax({
-                            url: '/getType/' + category_name,
-                            type: "GET",
-                            dataType: "json",
-                            success: function(data) {
-                                $('#type').empty().append(
-                                    '<option selected>Pilih Jenis Panel</option>');
-                                $.each(data, function(key, item) {
-                                    $('#type').append('<option value="' + item.id +
-                                        '" data-price="' + item.price + '">' + item
-                                        .type + '</option>');
-                                });
-                            },
-                            error: function(xhr, status, error) {
-                                console.log(xhr.responseText);
-                            }
-                        });
-                    } else {
-                        $('#type').empty().append('<option selected>Pilih Jenis Panel</option>');
-                    }
-                });
-
-                $('#type').on('change', function() {
-                    var selectedOption = $(this).find('option:selected');
-                    var price = parseFloat(selectedOption.data('price')) || 0;
-                    $('#price').text(price.toLocaleString()).data('price', price);
-                    $('#length, #width').trigger('input');
-                });
-
-                $('#length, #width').on('input', function() {
-                    var length = parseFloat($('#length').val()) || 0;
-                    var width = parseFloat($('#width').val()) || 0;
-                    var price = parseFloat($('#price').data('price')) || 0;
-                    var result = length * width * price;
-                    $('#resultValue').text(result.toLocaleString('id-ID'));
-                    $('#result_hidden').val(result.toFixed(2));
-                });
-
-
-                $('form').on('submit', function() {
-                    var total = $('#hasil_hidden').val();
-                    $('#hasil_hidden').val(parseFloat(total).toFixed(2));
-                });
-            });
-        </script>
-
-        <script>
-            $(document).ready(function() {
-                var successMessage = "{{ session('success') }}";
-                if (successMessage) {
-                    $('#successModal').modal('show');
+            $('#provinces').on('change', function() {
+                var province_id = $(this).val();
+                if (province_id) {
+                    $.ajax({
+                        url: "{{ route('getRegencies') }}",
+                        type: "POST",
+                        data: {
+                            province_id: province_id,
+                            _token: '{{ csrf_token() }}'
+                        },
+                        success: function(data) {
+                            $('#regencies').empty().append(
+                                '<option selected>Pilih Kabupaten</option>');
+                            $('#regencies').append(data);
+                        },
+                        error: function(xhr, status, error) {
+                            console.log(xhr.responseText);
+                        }
+                    });
+                } else {
+                    $('#regencies').empty().append('<option selected>Pilih Kabupaten</option>');
                 }
             });
-        </script>
 
-        <script>
-            document.getElementById('openAlertModal').addEventListener('click', function() {
-                document.getElementById('alertName').textContent = document.getElementById('name').value;
-                document.getElementById('alertWa').textContent = document.getElementById('wa').value;
-                document.getElementById('alertProvince').textContent = document.getElementById('provinces').options[
-                    document.getElementById('provinces').selectedIndex].text;
-                document.getElementById('alertRegency').textContent = document.getElementById('regencies').options[
-                    document.getElementById('regencies').selectedIndex]?.text || 'N/A';
-                document.getElementById('alertCategory').textContent = document.getElementById('category').options[
-                    document.getElementById('category').selectedIndex].text;
-                document.getElementById('alertPanel').textContent = document.getElementById('type').options[document
-                    .getElementById('type').selectedIndex].text;
-                document.getElementById('alertLength').textContent = document.getElementById('length').value;
-                document.getElementById('alertWidth').textContent = document.getElementById('width').value;
-                var totalResult = $('#resultValue').text();
-                document.getElementById('alertResult').textContent = totalResult;
-
-                $('#alertModal').modal('show');
+            $('#category').on('change', function() {
+                var category_name = $(this).val();
+                if (category_name) {
+                    $.ajax({
+                        url: '/getType/' + category_name,
+                        type: "GET",
+                        dataType: "json",
+                        success: function(data) {
+                            $('#type').empty().append(
+                                '<option selected>Pilih Jenis Panel</option>');
+                            $.each(data, function(key, item) {
+                                $('#type').append('<option value="' + item.id +
+                                    '" data-price="' + item.price + '">' + item
+                                    .type + '</option>');
+                            });
+                        },
+                        error: function(xhr, status, error) {
+                            console.log(xhr.responseText);
+                        }
+                    });
+                } else {
+                    $('#type').empty().append('<option selected>Pilih Jenis Panel</option>');
+                }
             });
 
-            document.getElementById('confirmOrder').addEventListener('click', function(e) {
-                e.preventDefault();
-                $('#alertModal').modal('hide');
-                document.getElementById('orderForm').submit();
+            $('#type').on('change', function() {
+                var selectedOption = $(this).find('option:selected');
+                var price = parseFloat(selectedOption.data('price')) || 0;
+                $('#price').text(price.toLocaleString()).data('price', price);
+                $('#length, #width').trigger('input');
             });
-        </script>
 
-    @endsection
+            $('#length, #width').on('input', function() {
+                var length = parseFloat($('#length').val()) || 0;
+                var width = parseFloat($('#width').val()) || 0;
+                var price = parseFloat($('#price').data('price')) || 0;
+                var result = length * width * price;
+                $('#resultValue').text(result.toLocaleString('id-ID'));
+                $('#result_hidden').val(result.toFixed(2));
+            });
+
+
+            $('form').on('submit', function() {
+                var total = $('#hasil_hidden').val();
+                $('#hasil_hidden').val(parseFloat(total).toFixed(2));
+            });
+        });
+    </script>
+
+    <script>
+        $(document).ready(function() {
+            var successMessage = "{{ session('success') }}";
+            if (successMessage) {
+                $('#successModal').modal('show');
+            }
+        });
+    </script>
+
+    <script>
+        document.getElementById('openAlertModal').addEventListener('click', function() {
+            document.getElementById('alertName').textContent = document.getElementById('name').value;
+            document.getElementById('alertWa').textContent = document.getElementById('wa').value;
+            document.getElementById('alertProvince').textContent = document.getElementById('provinces').options[
+                document.getElementById('provinces').selectedIndex].text;
+            document.getElementById('alertRegency').textContent = document.getElementById('regencies').options[
+                document.getElementById('regencies').selectedIndex]?.text || 'N/A';
+            document.getElementById('alertCategory').textContent = document.getElementById('category').options[
+                document.getElementById('category').selectedIndex].text;
+            document.getElementById('alertPanel').textContent = document.getElementById('type').options[document
+                .getElementById('type').selectedIndex].text;
+            document.getElementById('alertLength').textContent = document.getElementById('length').value;
+            document.getElementById('alertWidth').textContent = document.getElementById('width').value;
+            var totalResult = $('#resultValue').text();
+            document.getElementById('alertResult').textContent = totalResult;
+
+            $('#alertModal').modal('show');
+        });
+
+        document.getElementById('confirmOrder').addEventListener('click', function(e) {
+            e.preventDefault();
+            $('#alertModal').modal('hide');
+            document.getElementById('orderForm').submit();
+        });
+    </script>
+
+@endsection
